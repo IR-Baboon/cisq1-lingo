a6841061688e818a64186cd6cee418e7f988cc45
lexcomesdev <57563562+lexcomesdev@users.noreply.github.com>
2021-03-31 20:20:25 +0200

    fixed ci again :)

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -3,6 +3,7 @@ package nl.hu.cisq1.lingo.trainer.domain;
 import nl.hu.cisq1.lingo.trainer.domain.exceptions.InvalidFeedBackException;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Feedback {
     private String attempt;
@@ -45,4 +46,13 @@ public class Feedback {
         return String.valueOf(hint);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Feedback)) return false;
+        Feedback feedback = (Feedback) o;
+        return Objects.equals(attempt, feedback.attempt) &&
+                Objects.equals(marks, feedback.marks);
+    }
+    
 }
